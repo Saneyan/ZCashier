4e04ffed2c298051fac73ffdc4c1ff381179939e
Saneyuki Tadokoro <saneyan@mail.gfunction.com>
2013-05-21 14:50:32 +0900

    Add web server script.

---
+++ server/server.js
@@ -0,0 +1,18 @@
+﻿/**
+ * server.js
+ *
+ * @author   Saneyuki Tadokoro <saneyan@mail.gfunction.com>
+ * @preserve Copyright 2013, Saneyuki Tadokoro
+*/
+
+var http = require('http'),
+    jade = require('jade');
+
+var server = http.createServer();
+
+server.on('request', function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, world');
+});
+
+server.listen(80, '127.0.0.1');
